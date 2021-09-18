575fdd5a8e784f030345db4d44da9ee341a0ac9e
sajith udara perera <001sajith@gmail.com>
2021-09-18 12:27:40 +0530

    feat: Data binding example in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  appName = 'User Registration'
+  userName: string = ''
+  phoneNumber: string = ''
+  buttonDisable = true
+
+  users = []
+
+
+  addUser() {
+    console.log(this.buttonDisable)
+    this.users.push({
+      name: this.userName,
+      phoneNo: this.phoneNumber
+    })
+
+    this.userName = ''
+    this.phoneNumber = ''
+    this.validate()
+  }
+
+  validate() {
+    this.buttonDisable = (this.userName.length > 0 && this.phoneNumber.length > 0) ? false : true;
+  }
 }
